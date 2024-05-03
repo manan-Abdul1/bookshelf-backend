3c28e4240bae803e3fea23fd8c52de299627b42e
Abdul <manansheikh117@gmail.com>
2024-05-03 19:10:28 +0500

    Add databse connection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
+const dbconfig = require('./connection/db');
 
 const app = express();
 
