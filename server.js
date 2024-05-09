9e139ebbecec32b3a888b59b5fe25fde986c5f10
Abdul <manansheikh117@gmail.com>
2024-05-10 01:21:07 +0500

    Add a auth routes in server

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const cors = require('cors');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const dbconfig = require('./connection/db');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
