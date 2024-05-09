b40b34270da67f957f34b3930a6365cf79c82e74
Abdul <manansheikh117@gmail.com>
2024-05-10 00:49:42 +0500

    Update the addBookToShelf method with status

--- controllers/userBookController.js
+++ controllers/userBookController.js
@@ -3,26 +3,26 @@ const UserBook = require("../models/userBookSchema");
 // Controller to add Book To Shelf
 const addBookToShelf = async (req, res) => {
     try {
-        const { userId, bookId, shelfId } = req.body;
-
-        const newUserBook = new UserBook({
-            userId,
-            bookId,
-            shelfId,
-        });
-
-        await newUserBook.save();
-
-        res.status(201)
-            .json({
-                message: "Book added to shelf successfully",
-                newUserBook,
-                ok: true,
-            });
+      const { userId, bookId, shelfId, status } = req.body;
+  
+      const existingUserBook = await UserBook.findOne({ userId, bookId });
+      if (existingUserBook) {
+        return res.status(400).json({ message: "Book already exists in shelf" });
+      }
+  
+      const newUserBook = new UserBook({
+        userId,
+        bookId,
+        shelfId,
+        status, 
+      });
+      await newUserBook.save();
+  
+      res.status(201).json(newUserBook);
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Internal server error" });
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
     }
-};
-
+  };
+  
 module.exports = { addBookToShelf };
