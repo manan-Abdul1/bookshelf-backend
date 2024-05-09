7a49b4cfdc7b226509cca7918bed9e00146f3ba0
Abdul <manansheikh117@gmail.com>
2024-05-09 23:59:27 +0500

    Add a login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
-const bcrypt = require('bcrypt');
-const USER = require('../models/userSchema');
+const bcrypt = require("bcrypt");
+const USER = require("../models/userSchema");
 
 const signup = async (req, res) => {
     try {
@@ -7,7 +7,7 @@ const signup = async (req, res) => {
 
         const existingUser = await USER.findOne({ email });
         if (existingUser) {
-            return res.status(400).json({ message: 'User already exists' });
+            return res.status(400).json({ message: "User already exists" });
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -16,17 +16,48 @@ const signup = async (req, res) => {
             username,
             email,
             password: hashedPassword,
-            role
+            role,
         });
 
         await newUser.save();
 
-        res.status(201).json({ message: 'User registered successfully' });
+        res.status(201).json({ message: "User registered successfully" });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
+        res.status(500).json({ message: "Internal server error" });
     }
 };
 
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await USER.findOne({ email });
 
-module.exports = { signup };
+        if (!user) {
+            return res
+                .status(401)
+                .json({ message: "User does not exist!", ok: false });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            return res
+                .status(401)
+                .json({ message: "Enter the correct password", ok: false });
+        }
+
+        res
+            .status(200)
+            .json({
+                id: user._id,
+                username: user.username,
+                message: "Login successful",
+            });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+};
+
+module.exports = { signup, login };
