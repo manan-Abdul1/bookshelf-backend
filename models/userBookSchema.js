c286143a7df15cec18fa84724e6b06eef361a101
Abdul <manansheikh117@gmail.com>
2024-05-09 23:53:41 +0500

    Add a user book schema

---
+++ models/userBookSchema.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const UserBookSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  book: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Book',
+    required: true
+  },
+  shelf: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Shelf',
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['Reading', 'Completed', 'PlanToRead'],
+    default: 'PlanToRead'
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now
+  }
+});
+
+const UserBook = mongoose.model('UserBook', UserBookSchema);
+
+module.exports = UserBook;
