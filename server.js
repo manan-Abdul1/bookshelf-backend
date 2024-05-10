843d5742ecdd6fceb35ae322d43e2b42918c60be
Abdul <manansheikh117@gmail.com>
2024-05-11 02:28:38 +0500

    Add api genre routes in server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const dbconfig = require('./connection/db');
 const authRoutes = require('./routes/authRoutes');
+const genreRoutes = require('./routes/genreRoutes');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/genre', genreRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
